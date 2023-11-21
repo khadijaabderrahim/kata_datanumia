96967eedbcdc475bebc92921b16c8808d23ec2dd
Khadija ABDERRAHIM <khadija.abderrahim1991@gmail.com>
2023-11-21 09:29:26 +0100

    calcul score implementation

--- java/src/main/java/yatzy/service/CalculScoreVisitorImpl.java
+++ java/src/main/java/yatzy/service/CalculScoreVisitorImpl.java
@@ -1,6 +1,11 @@
 package yatzy.service;
 
+
 import yatzy.categories.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CalculScoreVisitorImpl implements CalculScoreVisitor {
     private static CalculScoreVisitorImpl instance;
@@ -16,51 +21,106 @@ public class CalculScoreVisitorImpl implements CalculScoreVisitor {
 
     @Override
     public int visit(Chance chance) {
-        return 0;
+        return chance.getDices().stream().mapToInt(Integer::intValue).sum();
     }
 
     @Override
     public int visit(FourOfKind fourOfKind) {
-        return 0;
+        return fourOfKind.getDices().stream().collect(Collectors.groupingBy(d -> d))
+            .entrySet()
+            .stream()
+            .map(d -> Map.entry(d.getKey(), d.getValue().size()))
+            .filter(d -> d.getValue() >= 4)
+            .map(Map.Entry::getKey)
+            .map(d -> d * 4)
+            .findAny()
+            .orElse(0);
     }
 
     @Override
     public int visit(Pair pair) {
-        return 0;
+        return pair.getDices().stream().collect(Collectors.groupingBy(d -> d))
+            .entrySet()
+            .stream()
+            .map(d -> Map.entry(d.getKey(), d.getValue().size()))
+            .filter(d -> d.getValue() >= 2)
+            .mapToInt(Map.Entry::getKey)
+            .map(d -> d * 2)
+            .max()
+            .orElse(0);
     }
 
     @Override
     public int visit(Single single) {
-        return 0;
+        return single.getDices().stream().filter(d -> d.equals(single.getSingleNumber())).mapToInt(Integer::intValue).sum();
+
     }
 
     @Override
     public int visit(ThreeOfKind threeOfKind) {
-        return 0;
+        return threeOfKind.getDices().stream().collect(Collectors.groupingBy(d -> d))
+            .entrySet()
+            .stream()
+            .map(d -> Map.entry(d.getKey(), d.getValue().size()))
+            .filter(d -> d.getValue() >= 3)
+            .map(Map.Entry::getKey)
+            .map(d -> d * 3)
+            .findAny()
+            .orElse(0);
     }
 
     @Override
     public int visit(TwoPairs twoPairs) {
-        return 0;
+        List<Integer> distinctPairs = twoPairs.getDices().stream().collect(Collectors.groupingBy(d -> d))
+            .entrySet()
+            .stream()
+            .map(d -> Map.entry(d.getKey(), d.getValue().size()))
+            .filter(d -> d.getValue() >= 2)
+            .map(Map.Entry::getKey)
+            .toList();
+        if (distinctPairs.size() != 2)
+            return 0;
+        return distinctPairs.stream().mapToInt(Integer::intValue).sum() * 2;
     }
 
     @Override
     public int visit(Yatzy yatzy) {
-        return 0;
+        if (yatzy.getDices().stream().distinct().count() == 1)
+            return 50;
+        else
+            return 0;
     }
 
     @Override
     public int visit(SmallStraight smallStraight) {
-        return 0;
+        List<Integer> smallStraightOk = List.of(1, 2, 3, 4, 5);
+        return smallStraightOk.stream()
+            .filter(d -> !smallStraight.getDices().contains(d))
+            .findAny()
+            .map(d -> 0)
+            .orElse(15);
     }
 
     @Override
     public int visit(LargeStraight largeStraight) {
-        return 0;
+        List<Integer> largeStraightOk = List.of(2, 3, 4, 5, 6);
+        return largeStraightOk.stream()
+            .filter(d -> !largeStraight.getDices().contains(d))
+            .findAny()
+            .map(d -> 0)
+            .orElse(20);
     }
 
     @Override
     public int visit(FullHouse fullHouse) {
-        return 0;
+        Map<Integer, List<Integer>> groupedDices = fullHouse.getDices().stream()
+            .collect(Collectors.groupingBy(d -> d));
+        if (groupedDices.size() != 2)
+            return 0;
+        return groupedDices
+            .entrySet()
+            .stream()
+            .mapToInt(e -> e.getKey() * e.getValue().size())
+            .sum();
     }
 }
